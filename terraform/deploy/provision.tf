b19b04101d8bdfd883059cbbe30866847442a559
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2025-11-19 15:16:47 +0100

    edit helm provider cred config

--- terraform/deploy/provision.tf
+++ terraform/deploy/provision.tf
@@ -79,8 +79,9 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = azurerm_kubernetes_cluster.time_api_cluster.kube_admin_config[0].host
+
     client_certificate     = base64decode(azurerm_kubernetes_cluster.time_api_cluster.kube_admin_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.time_api_cluster.kube_admin_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.time_api_cluster.kube_admin_config[0].cluster_ca_certificate)
