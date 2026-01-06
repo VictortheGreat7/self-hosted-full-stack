e4ae8563863aef797fe55536a65a8265a971baa2
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-06 16:10:17 +0100

    Without requests module

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_client import Counter, Gauge, Histogram
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
+# from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
