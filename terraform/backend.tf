583e266073d4988b2403349667012eca53d9a1a8
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-06 15:29:04 +0100

    Add new backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 # This file is used to configure the backend for the terraform state file.
 terraform {
   backend "azurerm" {
-    resource_group_name  = "backend-rg"
-    storage_account_name = "bunnybackend349"
+    resource_group_name  = "tfbackend-rg"
+    storage_account_name = "kronos349"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
   }
