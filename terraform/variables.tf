83ec5873ce2209cf928d55d78ade238d5fd2571c
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2026-01-06 15:10:04 +0100

    Add variables for ssl certification and az sub

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,13 +6,44 @@ variable "region" {
   default     = "eastus"
 }
 
+variable "azure_subscription" {
+  description = "The Azure Subscription ID"
+  type        = string
+  sensitive   = true
+}
+
 variable "my_user_object_id" {
   description = "The object id of the user"
   type        = string
+  sensitive   = true
 }
 
 variable "github_runner_token" {
   description = "GitHub Actions runner registration token"
   type        = string
   sensitive   = true
+}
+
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token for managing DNS records"
+  type        = string
+  sensitive   = true
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID for the domain"
+  type        = string
+  sensitive   = true
+}
+
+variable "subdomain" {
+  description = "The subdomain to use for the world clock application"
+  type        = string
+  default     = "kronos"
+}
+
+variable "domain" {
+  description = "The root domain for the world clock application"
+  type        = string
+  default     = "mywonderworks.tech"
 }
